c0ee1eb3f4f2836079beb37197b2ee5927db1d93
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-01-22 14:19:56 -0500

    Finish M1Lab_Craig.cpp

--- M1Lab_Craig.cpp
+++ M1Lab_Craig.cpp
@@ -4,21 +4,25 @@ M1LAB - Apple sales
 Rome Craig
 1/22/24
 */
-#include <isostream>
+#include <iostream>
 using namespace std;
 
 int main() {
     // apple sales program
-    string name = 'Your Name';
-    int num_apples = 100;
+    string name = "Rome";
+    int num_apples = 97;
     double price_per_apple = 0.25;
 
     // cal price for all apples
     // TODO
+    double total_price = num_apples * price_per_apple;
 
     // print the output
     cout << "Welcome to the "<< name << "Apple Farm. " << endl;
     cout << "We have " << num_apples << " apples." << endl;
+    cout << "Each on cost $" << price_per_apple << endl;
+    cout << "For a total cost of $" << total_price << endl;
+    cout << endl;
 
     return 0;
 }
