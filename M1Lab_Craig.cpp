79beb9c1dbd6d96214166d41963af906a7e3f424
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-01-22 14:18:32 -0500

    m1lab

---
+++ M1Lab_Craig.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+M1LAB - Apple sales
+Rome Craig
+1/22/24
+*/
+#include <isostream>
+using namespace std;
+
+int main() {
+    // apple sales program
+    string name = 'Your Name';
+    int num_apples = 100;
+    double price_per_apple = 0.25;
+
+    // cal price for all apples
+    // TODO
+
+    // print the output
+    cout << "Welcome to the "<< name << "Apple Farm. " << endl;
+    cout << "We have " << num_apples << " apples." << endl;
+
+    return 0;
+}
