235e9b83bfbcdd3fb0c0c8a2a4856dfc6a51554e
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-01-31 19:29:45 +0000

    box sales

--- M2Lab1_craig.cpp
+++ M2Lab1_craig.cpp
@@ -6,19 +6,45 @@ using namespace std;
 int main() {
     double len;
     double wid;
-    double area;
+    double hieg;
+    double volume;
+    // contants dont change
+    // cost_per_sqaure foot
+    // charge per cubic foot
+    const double costpcf = 0.23;
+    const double cpcf = 0.5;
+    // math stuff
+    double cost;
+    double charge;
+    double profit;
 
-    cout << "What's the length of the rectangle?  ";
+    cout<< "M1T2 - Box corp";
+
+    cout << "What's the length?  ";
     cin >> len;
-    cout << "What's the width of the rectangle?  ";
+    cout << "What's the width?  ";
     cin >> wid;
+    cout << "What's the height? ";
+    cin >> hieg;
     
-    area = len * wid;
+    volume = len * wid * hieg;
+    cost = volume * costpcf;
+    charge = volume * cpcf;
+    profit = charge - cost;
 
-    cout << "The area of the rectangle is: " << area << endl;
+    cout << "The Volume of the rectangle is: " << volume << endl;
+    cout << "The cost to build it is $" << cost << endl;
+    cout << "You can charge it to customers for $" << charge << endl;
+    cout << "You will make $" << profit << " in profit" << endl;
 
 
-    return 0;
+
+
+   
+
+
+   return 0;
+   
 }
 
 
