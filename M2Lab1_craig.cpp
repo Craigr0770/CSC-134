009a9d0cc6f4619c47c060bb6401035bf542c3b6
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-01-31 18:42:34 +0000

    2d

---
+++ M2Lab1_craig.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double len;
+    double wid;
+    double area;
+
+    cout << "What's the length of the rectangle?  ";
+    cin >> len;
+    cout << "What's the width of the rectangle?  ";
+    cin >> wid;
+    
+    area = len * wid;
+
+    cout << "The area of the rectangle is: " << area << endl;
+
+
+    return 0;
+}
+
+
+
