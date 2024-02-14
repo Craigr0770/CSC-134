335105b62b5e9d8668e185bebc7eebc78eb49b60
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-02-14 19:46:56 +0000

    hw

--- M2HW1.cpp
+++ M2HW1.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
-
+void Question1();
+void Question2();
 int main() {
+    Question1();
+    Question2();
+
+    return 0;
+}
+
+
+void Question1(){
     double blance;
     double Nblance;
     string Uname;
@@ -50,6 +59,51 @@ int main() {
 
 
 
-    return 0;
+    
    
+}
+
+void Question2(){ 
+cout << "----------" << endl;
+double numpizza;
+double pizzaslices;
+double numpeople;
+double pizzaeat;
+double pizzaleft;
+string pizzatype;
+
+
+
+cout << "Welcome to RC pizzaria" << endl;
+
+cout << "Small pizza 4 slices Medium 6 slices Large 8 slices " << endl;
+cout << "What type of pizza do you want ? " <<endl;
+cout << " Enter small , medium , or large : ";
+cin >> pizzatype;
+
+if (pizzatype == "small"){
+    cout << "How many pizzas are you getting?" << endl;
+cin >> numpizza;
+pizzaslices = numpizza * 4;
+}
+else if (pizzatype == "medium"){
+    cout << "How many pizzas are you getting?" << endl;
+cin >> numpizza;
+pizzaslices = numpizza * 6;
+}
+else  if ( pizzatype == "large"){
+    cout << "How many pizzas are you getting?" << endl;
+cin >> numpizza;
+pizzaslices = numpizza * 8;
+}
+
+
+
+cout << "How many peaople are eating pizza? ";
+cin >> numpeople;
+pizzaeat = numpeople * 3;
+pizzaleft = pizzaslices - pizzaeat;
+cout << "You will have " << pizzaleft << " pizza slices left.";
+
+
 }
