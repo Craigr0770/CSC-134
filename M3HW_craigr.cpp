9e20346620f4bf3da2762753c92adcd44f838178
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-02-26 19:40:43 +0000

    turtle 1

---
+++ M3HW_craigr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string ans;
+
+    cout << "Hello, im Rome's C++ program" << endl;
+    cout << "Do you like pizza? ";
+    cout << "Please type Yes or No"
+    cin >> ans;
+    if (ans = "Yes"){
+        cout << "Your a cool turtle"<< endl;
+    }
+
+
+
+
+
+    return 0;
+   
+}
