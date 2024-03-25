3d5a207d054c09646927ce2a7c26d9b38b1e832b
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2024-03-25 18:37:27 +0000

    done

---
+++ M4T1_craig.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+    for (int i = 1; i <= 5; ++i) {
+        cout <<  "Hello World! " << endl; }
+    
+
+     for (int i = 1; i <= 5; ++i) {
+        cout <<  "Hello World! " << endl;
+
+    const int MIN_NUMBER =1,
+        MAX_NUMBER=10;
+
+    int num = MIN_NUMBER;
+    cout << "Number Number Squared\n";
+    cout << "------------------\n";
+    while (num <= MAX_NUMBER)
+    {
+        cout << num << "\t\t" << (num * num) << endl;
+        num++;
+    }
+    
+
+
+
+
+    }
+    
+        
+    
+
+
+
+
+    return 0;
+   
+}
